d0acc7362fed104513f7144b9931f3639e9263f3
DevKyle29 <kylebrentfry@gmail.com>
2023-02-07 17:46:25 +0200

    In Progress
    Enjoying it

---
+++ Basic Quiz Game.js
@@ -0,0 +1,19 @@
+let correctAnswers = [];
+
+let correctQuestions = ['Calf','Cub','Kid','Foal','Kitten','Hatchling','Pup','Baby'];
+
+let questions1 = prompt('Select the correct Anwser A:Calf B:Cria C:Infant');
+
+let questions2 = prompt('Select the correct Anwser A:Calf B:Cub C:Infant');
+
+let questions3 = prompt('Select the correct Anwser A:Calf B:Cria C:Kid');
+
+let questions4 = prompt('Select the correct Anwser A:Foal B:Cria C:Infant');
+
+let questions5 = prompt('Select the correct Anwser A:Calf B:Kitten C:Infant');
+
+let questions6 = prompt('Select the correct Anwser A:Calf B:Cria C:Hatchling');
+
+let questions7 = prompt('Select the correct Anwser A:Calf B:Pup C:Infant');
+
+let questions8 = prompt('Select the correct Anwser A:Baby B:Cria C:Infant');
